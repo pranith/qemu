9c72a3064da1a8085fd0b3dc523fcc91d0d214b1
Pranith Kumar <bobby.prani@gmail.com>
2015-05-22 17:25:12 -0400

    Add magic cb support for app start/end

--- vl.c
+++ vl.c
@@ -262,7 +262,8 @@ bool qsim_gen_callbacks = false;
 uint64_t	qsim_host_addr;
 uint64_t	qsim_phys_addr;
 
-uint64_t    qsim_icount = 2040000000;
+uint64_t    qsim_icount = 10000000;
+bool        call_magic_cb = false;
 
 qsim_ucontext_t main_context;
 qsim_ucontext_t qemu_context;
@@ -1967,7 +1968,18 @@ static void qsim_loop_main(void)
         dev_time += profile_getclock() - ti;
 #endif
     } while (!main_loop_should_exit());
+    bdrv_close_all();
 	pause_all_vcpus();
+    res_free();
+#ifdef CONFIG_TPM
+    tpm_cleanup();
+#endif
+
+    // send shutdown signal
+    if (qsim_magic_cb)
+        qsim_magic_cb(0, 0xfa11dead);
+
+    return;
 }
 
 static void version(void)
@@ -4829,6 +4841,5 @@ int qsim_qemu_main(int argc, const char **argv, char **envp)
         }
     }
 
-    res_free();
     return 0;
 }
