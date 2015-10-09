c79eabdf146d0862dbb66e71aafabf9c402d94f2
Pranith Kumar <bobby.prani@gmail.com>
2015-10-09 10:18:55 -0400

    Update qsim API to version 0.15.1
    Also fix build issues with integrated qsim.

--- exec.c
+++ exec.c
@@ -182,6 +182,37 @@ struct CPUAddressSpace {
 
 #if !defined(CONFIG_USER_ONLY)
 
+uint8_t         qsim_irq_vec     = 0;
+int             qsim_irq_pending = 0;
+static pthread_mutex_t qsim_irq_lock    = PTHREAD_MUTEX_INITIALIZER;
+
+
+int interrupt(uint8_t vec) {
+  int rvec = 0;
+
+  pthread_mutex_lock(&qsim_irq_lock);
+  if (qsim_irq_pending == 1  && qsim_irq_vec < vec) {
+    rvec = qsim_irq_vec;
+    qsim_irq_vec = vec;
+  } else if (qsim_irq_pending == 0) {
+    rvec = -1;
+    qsim_irq_vec = vec;
+    qsim_irq_pending = 1;
+  } else {
+    rvec = vec;
+  }
+
+  // Re-notify the CPU no matter what.
+  cpu_interrupt(first_cpu, CPU_INTERRUPT_HARD);
+  //qemu_notify_event();
+  pthread_mutex_unlock(&qsim_irq_lock);
+
+  // Give the caller the vector number of an interrupt that _won't_ be
+  // processed and needs to be queued if it is to be handled, or -1.
+  return rvec;
+}
+
+
 static void phys_map_node_reserve(PhysPageMap *map, unsigned nodes)
 {
     if (map->nodes_nb + nodes > map->nodes_nb_alloc) {
