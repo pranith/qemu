db4ddc0996f69d7196ec3debef2de734db89a513
Trey Cain <hcain@marvell.com>
2019-08-06 08:04:41 -0700

    Changes to some of the icount parameters -- this one seems to result in about 2% EL1 activity when generating a trace of a user-only workload. Hopefully good enough for now.

--- txscripts/boot.sh
+++ txscripts/boot.sh
@@ -6,5 +6,7 @@
                 -device scsi-hd,drive=coreimg -netdev user,id=unet,hostfwd=tcp::2223-:22 \
                 -device virtio-net-device,netdev=unet -kernel ../images/vmlinuz-5.0.0-8-generic -initrd ../images/initrd.img-5.0.0-8-generic -display sdl -nographic \
                 -append "root=/dev/sda2 lpj=34920500 notsc nowatchdog rcupdate.rcu_cpu_stall_suppress=1" \
-		-icount 1,align=off,shift=3,sleep=off -rtc "clock=vm"
-                -plugin "file=../plugins/perfsim-log/perfsim-log.so"
+		-icount align=off,shift=4,sleep=on -rtc "clock=vm" \
+                -plugin "file=../plugins/perfsim-log/perfsim-log.so" \
+                -fsdev local,id=p9test,path=/t99/cadv21/archcommon/,security_model=none -device virtio-9p-pci,fsdev=p9test,mount_tag=test_mount
+
